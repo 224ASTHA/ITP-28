da4db80d4d5c5c31d1b4c21accb83fc6532a10ce
subham2006-lab <subhamtripathy926@gmail.com>
2025-03-31 11:12:38 +0530

    Update q2.cpp

--- week1/subhamtripathy/q2.cpp
+++ week1/subhamtripathy/q2.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int a,b,c;
+    cout << "Enter three sides of a triangle:" << endl;
+    cin >> a >> b >> c;
+    if((a+b)>c && (a+c)>b && (b+c)>a){
+        if(a==b && b==c && c==a)
+        cout << "Triangle is equilateral";
+        else if(a==b || b==c || c==a)
+        cout << "Triangle is isosceles";
+        else 
+        cout << "Triangle is scalene";
+    } else{
+        cout << "A triangle can't be formed from the entered details"<<endl;
+    }
+    return 0;
+}
